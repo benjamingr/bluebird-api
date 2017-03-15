58b50efaa6bb6359772e079326e8065610ce4bcd
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 19:32:31 +0200

    props

--- promise.js
+++ promise.js
@@ -6,6 +6,22 @@ class Bluebird extends Promise {
             return value;
         })());
     }
+
+    props() {
+        return Bluebird.resolve((async () => {
+            const value = await this;
+            const ret = {};
+            if(typeof value !== "object" || value == null) {
+                throw new TypeError("Expected an object passed to `.props`, got " + typeof value + " instead");
+            }
+            const keys = Object.keys(value);
+            for(const key of keys) {
+                ret[key] = await value[key];
+            }
+            return ret;
+        })());
+    }
+    static props(o) { return Promise.resolve(o).props()}
 }
 
 module.exports = Bluebird;
---
+++ tests/props.js
@@ -0,0 +1,36 @@
+const assert = require("assert");
+const Promise = require("../promise.js");
+
+describe("props", () => {
+    
+    it("reads the props of a simple object ", async () => {
+        const o = {x:3, y: 5};
+        const o2 = await Promise.resolve(o).props();
+        assert.equal(o.x, 3);
+        assert.equal(o.y, 5);
+    });
+
+    it("reads the props of an object where one is awaitable", async () => {
+        const o = {x:3, y: Promise.resolve(5)};
+        const o2 = await Promise.resolve(o).props();
+        assert.equal(o.x, 3);
+        assert.equal(o.y, 5);
+    });
+
+    it("reads the props of an object where all are awaitable", async () => {
+        const o = {x: Promise.resolve(3), y: Promise.resolve(5)};
+        const o2 = await Promise.resolve(o).props();
+        assert.equal(o.x, 3);
+        assert.equal(o.y, 5);
+    });
+    
+    it("rejects when one of the props rejects", async () => {
+        const o = {x: Promise.reject(new Error("Hello")), y: Promise.resolve(5)};
+        try {
+            const o2 = await Promise.resolve(o).props();
+            assert.fail();  
+        } catch (e) {
+            assert(e.message === "Hello");
+        }
+    });
+});
