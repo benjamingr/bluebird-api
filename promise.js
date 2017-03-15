c78468618f88c612558eab913ec3e363dc00b894
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 19:49:00 +0200

    Done with props, fixes #16

--- promise.js
+++ promise.js
@@ -1,6 +1,5 @@
 class Bluebird extends Promise {}
 
-
 require('./promiseFns/tap')(Bluebird);
 require('./promiseFns/props')(Bluebird);
 require('./promiseFns/spread')(Bluebird);
--- promiseFns/props.js
+++ promiseFns/props.js
@@ -1,14 +1,11 @@
 module.exports = (Bluebird) => {
  Bluebird.prototype.props = function props() {
         return Bluebird.resolve((async () => {
-            const value = await this;
-            const ret = {};
-            console.log("Hello", value);
+            const ret = {}, value = await this;
             if(typeof value !== "object" || value == null) {
                 throw new TypeError("Expected an object passed to `.props`, got " + typeof value + " instead");
             }
-            const keys = Object.keys(value);
-            for(const key of keys) {
+            for(const key of  Object.keys(value)) {
                 ret[key] = await value[key];
             }
             return ret;
