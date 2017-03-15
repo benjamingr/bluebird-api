c2ddbb005e51c8e7dfa52479014de8058c192631
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 20:54:11 +0200

    first map test passes

--- promiseFns/map.js
+++ promiseFns/map.js
@@ -2,13 +2,13 @@ const util = require("util");
 module.exports = (Bluebird) => {
     Bluebird.map = (x, mapper, opts) => Bluebird.resolve(x).map(mapper, opts);
 
-    Bluebird.prototype.map = async function(mapper, {concurrency}) {
+    Bluebird.prototype.map = async function(mapper, {concurrency} = {}) {
         const values = Array.from(await this);
         if(!concurrency) {
-            return await Promise.all(values.map(fn));
+            return await Promise.all(values.map(mapper));
         }
-        const throttled = util.throttle(fn, Number(concurrency));
-        return await Promise.all(values.map(fn));
+        const throttled = util.throttle(mapper, Number(concurrency));
+        return await Promise.all(values.map(throttled));
     };
 };
 
--- tests/map.js
+++ tests/map.js
@@ -2,5 +2,8 @@ const assert = require("assert");
 const Promise = require("../promise.js");
 
 describe("map", () => {
-
+    it("maps sync stuff", async () => {
+        var arr = await Promise.map([1,2,3], x => x + 1);
+        assert.deepEqual(arr, [2,3,4]);
+    });
 });
