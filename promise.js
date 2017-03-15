7e7978ac61c147fd30de339dcf0560a7829ae295
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 19:02:24 +0200

    Add .tap, fixes #10

--- promise.js
+++ promise.js
@@ -1,4 +1,13 @@
+Promise.resolve([1,2,3]).get(0).then(console.log); // logs 1 
+
+Promise.resolve({x:3, y:5}).get('x').then(console.log); // logs 3
+
 class Bluebird extends Promise {
+    tap(onFulfilled) {
+        const value = await this;
+        await onFulfilled(value);
+        return value;
+    }
 }
 
 module.exports = Bluebird;
