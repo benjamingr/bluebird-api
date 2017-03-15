95e4a863aa460d5e25739711937c43c547e8ec66
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 21:23:27 +0200

    add .finally, closes #1

--- promise.js
+++ promise.js
@@ -8,8 +8,10 @@ require('./promiseFns/join')(Bluebird);
 require('./promiseFns/try')(Bluebird);
 require('./promiseFns/method')(Bluebird);
 require('./promiseFns/each')(Bluebird);
+require('./promiseFns/mapSeries')(Bluebird);
 require('./promiseFns/map')(Bluebird);
 require('./promiseFns/call')(Bluebird);
+require('./promiseFns/finally')(Bluebird);
 
 module.exports = Bluebird;
 
