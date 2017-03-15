82305cd4349a173f886b2668286c6da449dc5891
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 19:12:32 +0200

    fix idiot code by myself

--- promise.js
+++ promise.js
@@ -1,9 +1,5 @@
-Promise.resolve([1,2,3]).get(0).then(console.log); // logs 1 
-
-Promise.resolve({x:3, y:5}).get('x').then(console.log); // logs 3
-
 class Bluebird extends Promise {
-    tap(onFulfilled) {
+    async tap(onFulfilled) {
         const value = await this;
         await onFulfilled(value);
         return value;
---
+++ tests/tap.js
@@ -0,0 +1,12 @@
+const assert = require("assert");
+const Promise = require("../promise.js");
+
+describe("tap", () => {
+    it("taps a promise", async function() {
+        const p = Promise.resolve("hello");
+        let val = "";
+        const returnValue = await p.tap(inner => inner + " world");
+        assert.equal(val, "hello world");
+        assert.equal(returnValue, "hello");
+    });
+});
