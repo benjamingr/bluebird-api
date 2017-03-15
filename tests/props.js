e4350d4b86f5d26993c4656a05556e8098192a4c
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-15 19:34:23 +0200

    props basic tests

--- tests/props.js
+++ tests/props.js
@@ -2,26 +2,26 @@ const assert = require("assert");
 const Promise = require("../promise.js");
 
 describe("props", () => {
-    
+
     it("reads the props of a simple object ", async () => {
         const o = {x:3, y: 5};
         const o2 = await Promise.resolve(o).props();
-        assert.equal(o.x, 3);
-        assert.equal(o.y, 5);
+        assert.equal(o2.x, 3);
+        assert.equal(o2.y, 5);
     });
 
     it("reads the props of an object where one is awaitable", async () => {
         const o = {x:3, y: Promise.resolve(5)};
         const o2 = await Promise.resolve(o).props();
-        assert.equal(o.x, 3);
-        assert.equal(o.y, 5);
+        assert.equal(o2.x, 3);
+        assert.equal(o2.y, 5);
     });
 
     it("reads the props of an object where all are awaitable", async () => {
         const o = {x: Promise.resolve(3), y: Promise.resolve(5)};
         const o2 = await Promise.resolve(o).props();
-        assert.equal(o.x, 3);
-        assert.equal(o.y, 5);
+        assert.equal(o2.x, 3);
+        assert.equal(o2.y, 5);
     });
     
     it("rejects when one of the props rejects", async () => {
