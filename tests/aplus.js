46b5458adb01bef6f285e06d737e4387d8aba776
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-18 21:44:57 +0200

    promises/a+ tests pass, closes #13

---
+++ tests/aplus.js
@@ -0,0 +1,27 @@
+// promises/a+ suite.
+// run with node/tests/aplus.js - not a mocha file
+const promisesAplusTests = require("promises-aplus-tests");
+const Promise = require("../promise.js");
+
+
+// needed for aplus
+process.on("unhandledRejection", (e, v) => {}); 
+
+describe("Promises/A+ Tests", function () {
+    require("promises-aplus-tests").mocha({
+    resolved(v) { return Promise.resolve(v); },
+    rejected(e) { return Promise.reject(e); },
+    deferred() {
+        let resolve, reject;
+        let promise = new Promise((res, rej) => {
+            resolve = res;
+            reject = rej;
+        });
+        return {
+            promise,
+            resolve,
+            reject
+        };
+    }
+});
+});
