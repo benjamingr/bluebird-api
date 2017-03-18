a8a129d8c387f6a2215abc0ba4f79492f79a26dc
Benjamin Gruenbaum <benji@tipranks.com>
2017-03-18 22:04:15 +0200

    addNewLibraryCopy

--- promise.js
+++ promise.js
@@ -1,32 +1,38 @@
-class Bluebird extends Promise {}
-Bluebird.TypeError = TypeError; // Bluebird.TypeError is used in tests
+function factory() {
 
-require('./promiseFns/tap')(Bluebird);
-require('./promiseFns/props')(Bluebird);
-require('./promiseFns/spread')(Bluebird);
-require('./promiseFns/promisify')(Bluebird);
-require('./promiseFns/join')(Bluebird);
-require('./promiseFns/try')(Bluebird);
-require('./promiseFns/method')(Bluebird);
-require('./promiseFns/each')(Bluebird);
-require('./promiseFns/mapSeries')(Bluebird);
-require('./promiseFns/map')(Bluebird);
-require('./promiseFns/get')(Bluebird);
-require('./promiseFns/call')(Bluebird);
-require('./promiseFns/reflect')(Bluebird);
-require('./promiseFns/finally')(Bluebird);
-require('./promiseFns/return')(Bluebird);
-require('./promiseFns/reduce')(Bluebird);
-require('./promiseFns/throw')(Bluebird);
-require('./promiseFns/catchReturn')(Bluebird);
-require('./promiseFns/catchThrow')(Bluebird);
-require('./promiseFns/reflect')(Bluebird);
-require('./promiseFns/catch')(Bluebird);
-require('./promiseFns/coroutine')(Bluebird);
-require('./promiseFns/cast')(Bluebird);
-require('./promiseFns/fromCallback')(Bluebird);
-require('./promiseFns/setScheduler')(Bluebird);
-require('./promiseFns/asCallback')(Bluebird);
+    class Bluebird extends Promise {}
+    Bluebird.TypeError = TypeError; // Bluebird.TypeError is used in tests
+
+    require('./promiseFns/tap')(Bluebird);
+    require('./promiseFns/props')(Bluebird);
+    require('./promiseFns/spread')(Bluebird);
+    require('./promiseFns/promisify')(Bluebird);
+    require('./promiseFns/join')(Bluebird);
+    require('./promiseFns/try')(Bluebird);
+    require('./promiseFns/method')(Bluebird);
+    require('./promiseFns/each')(Bluebird);
+    require('./promiseFns/mapSeries')(Bluebird);
+    require('./promiseFns/map')(Bluebird);
+    require('./promiseFns/get')(Bluebird);
+    require('./promiseFns/call')(Bluebird);
+    require('./promiseFns/reflect')(Bluebird);
+    require('./promiseFns/finally')(Bluebird);
+    require('./promiseFns/return')(Bluebird);
+    require('./promiseFns/reduce')(Bluebird);
+    require('./promiseFns/throw')(Bluebird);
+    require('./promiseFns/catchReturn')(Bluebird);
+    require('./promiseFns/catchThrow')(Bluebird);
+    require('./promiseFns/reflect')(Bluebird);
+    require('./promiseFns/catch')(Bluebird);
+    require('./promiseFns/coroutine')(Bluebird);
+    require('./promiseFns/cast')(Bluebird);
+    require('./promiseFns/fromCallback')(Bluebird);
+    require('./promiseFns/setScheduler')(Bluebird);
+    require('./promiseFns/asCallback')(Bluebird);
+    return Bluebird;
+}
+const copy = factory();
+copy.getNewLibraryCopy = factory;
 
 module.exports = Bluebird;
 
---
+++ promiseFns/noConflict.js
@@ -0,0 +1,4 @@
+module.exports = (Bluebird) => {
+    return new Bluebird.copy;
+    // actual code in promise.js, this here for consistency
+};
