d73279439a84e09204b8afc9bfbc35b5ca7d87c6
Donald L Mull Jr <donald.mull@gmail.com>
2017-03-21 22:13:51 -0400

    Add typed catch filter to catchThrow and catchReturn

--- promiseFns/catchReturn.js
+++ promiseFns/catchReturn.js
@@ -1,5 +1,7 @@
 module.exports = (Bluebird) => {
     Bluebird.prototype.catchReturn = function catchReturn (value) {
-        return this.catch(() => value);
+        const filters = Array.prototype.slice.call(arguments, 0, -1);
+        
+        return this.catch.apply(this, [ ...filters, () => value ]);
     }
 };
